a3aaa7c723b72a3f6ed3ece1600151e248fdc9e1
Camille Emefa Acey <camilleacey@users.noreply.github.com>
2017-08-29 16:21:30 -0400

    Updating the exporter script for V2 beta

--- exporter.sh
+++ exporter.sh
@@ -2,36 +2,73 @@
 
 mkdir -p ./data
 
-echo "Saving all users to data/users.json"
-curl -X GET -H "Content-Type: application/json" \
-  -L "https://api.clubhouse.io/api/v1/users?token=$CLUBHOUSE_API_TOKEN" > data/users.json 2>/dev/null
+echo "Saving all Categories to data/categories.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/categories?token=$CLUBHOUSE_API_TOKEN" > data/categories.json 2>/dev/null
 
-echo "Saving all projects to data/projects.json"
-curl -X GET -H "Content-Type: application/json" \
-  -L "https://api.clubhouse.io/api/v1/projects?token=$CLUBHOUSE_API_TOKEN" > data/projects.json 2>/dev/null
+echo "Saving all Epics to data/epics.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/epics?token=$CLUBHOUSE_API_TOKEN" > data/epics.json 2>/dev/null
+  
+echo "Saving all Files to data/files.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/files?token=$CLUBHOUSE_API_TOKEN" > data/files.json 2>/dev/null
+  
+echo "Saving all Labels to data/labels.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/labels?token=$CLUBHOUSE_API_TOKEN" > data/labels.json 2>/dev/null
 
-echo "Saving all epics to data/epics.json"
-curl -X GET -H "Content-Type: application/json" \
-  -L "https://api.clubhouse.io/api/v1/epics?token=$CLUBHOUSE_API_TOKEN" > data/epics.json 2>/dev/null
+echo "Saving all Linked Files to data/linkedfiles.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/linked-files?token=$CLUBHOUSE_API_TOKEN" > data/linkedfiles.json 2>/dev/null
+  
+echo "Saving all Members to data/members.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/members?token=$CLUBHOUSE_API_TOKEN" > data/members.json 2>/dev/null
+  
+echo "Saving all Milestones to data/milestones.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/milestones?token=$CLUBHOUSE_API_TOKEN" > data/milestones.json 2>/dev/null
 
-echo "Saving workflow states to data/workflow.json"
-curl -X GET -H "Content-Type: application/json" \
-  -L "https://api.clubhouse.io/api/v1/workflows?token=$CLUBHOUSE_API_TOKEN" > data/workflow.json 2>/dev/null
+echo "Saving all Projects to data/projects.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/projects?token=$CLUBHOUSE_API_TOKEN" > data/projects.json 2>/dev/null
 
-echo "Saving all labels to data/labels.json"
-curl -X GET -H "Content-Type: application/json" \
-  -L "https://api.clubhouse.io/api/v1/labels?token=$CLUBHOUSE_API_TOKEN" > data/labels.json 2>/dev/null
+echo "Saving all Repositories to data/repositories.json"
+curl -X GET \
+  -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/projects?token=$CLUBHOUSE_API_TOKEN" > data/repositories.json 2>/dev/null
 
-echo "Saving all features to data/story.features.json"
+echo "Saving all Feature Stories to data/stories.features.json"
 curl -X POST -H "Content-Type: application/json" -d '{ "story_type": "feature" }' \
-  -L "https://api.clubhouse.io/api/v1/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/story.features.json 2>/dev/null
+  -L "https://api.clubhouse.io/api/beta/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/stories.features.json 2>/dev/null
 
-echo "Saving all bugs to data/story.bugs.json"
+echo "Saving all Bug Stories to data/stories.bugs.json"
 curl -X POST -H "Content-Type: application/json" -d '{ "story_type": "bug" }' \
-  -L "https://api.clubhouse.io/api/v1/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/story.bugs.json 2>/dev/null
+  -L "https://api.clubhouse.io/api/beta/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/stories.bugs.json 2>/dev/null
 
-echo "Saving all chores to data/story.chores.json"
+echo "Saving all Chore Stories to data/stories.chores.json"
 curl -X POST -H "Content-Type: application/json" -d '{ "story_type": "chore" }' \
-  -L "https://api.clubhouse.io/api/v1/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/story.chores.json 2>/dev/null
+  -L "https://api.clubhouse.io/api/beta/stories/search?token=$CLUBHOUSE_API_TOKEN" > data/stories.chores.json 2>/dev/null
+
+echo "Saving all Story-Links to data/storylinks.json"
+curl -X POST -H "Content-Type: application/json" -d '{ "story_type": "chore" }' \
+   -L "https://api.clubhouse.io/api/beta/story-links?token=$CLUBHOUSE_API_TOKEN" > data/storylinks.json 2>/dev/null
+
+echo "Saving all Teams to data/teams.json"
+curl -X POST -H "Content-Type: application/json" -d '{ "story_type": "chore" }' \
+  -L "https://api.clubhouse.io/api/beta/teams?token=$CLUBHOUSE_API_TOKEN" > data/teams.json 2>/dev/null
+
+echo "Saving Workflow states to data/workflows.json"
+curl -X GET -H "Content-Type: application/json" \
+  -L "https://api.clubhouse.io/api/beta/workflows?token=$CLUBHOUSE_API_TOKEN" > data/workflow.json 2>/dev/null
 
 echo "Done!"
